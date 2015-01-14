517a3742527bc5298ab1502447d522744bb1e81d
Matthew Wong <mtthwwng@gmail.com>
2015-01-13 19:12:57 -0800

    favicon, metadata, footer

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link href="css/styles.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="css/favicon.png" />
+
+    <!-- Metadata -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="keywords" content="Splunk Users"/>
+    <meta name="description" content="Splunk Users is a one-stop site for viewing all the information you need as a splunk user. "/>
+    <meta name="author" content="Matthew Wong">
     
 </head>
 <body>
@@ -92,6 +101,18 @@
                 <?php include 'wiki.php';?>
             </div>
 
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 text-center bottom-space">
+                <hr>
+                Copyright &copy; 
+                <script type="text/javascript">
+                    document.write(new Date().getFullYear());
+                </script>,
+                 Splunk Inc. Created by <a href="http://therealmatt.com/" target="_blank" rel="designer">Matthew Wong</a>.
+
+            </div>
         </div>
     </div> <!-- Fluid Container -->
 
